b4ec5ff32fcafd449f1e7a4685daff56def854f2
Dev-Mriganka <mrigankamondal10@gmail.com>
2022-11-08 21:22:21 +0530

    First Commit

---
+++ OnlineP_Food_Delivery_App/src/main/java/com/healthyswad/OnlinePFoodDeliveryAppApplication.java
@@ -0,0 +1,13 @@
+package com.healthyswad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePFoodDeliveryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePFoodDeliveryAppApplication.class, args);
+	}
+
+}
---
+++ OnlineP_Food_Delivery_App/src/test/java/com/healthyswad/OnlinePFoodDeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthyswad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePFoodDeliveryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
