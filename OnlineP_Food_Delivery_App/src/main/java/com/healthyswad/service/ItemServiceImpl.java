7f53cb8fb56c7b253c895eff6ea9b673709d35c8
Ankur0661 <ankurk171@gmail.com>
2022-11-09 21:02:01 +0530

    Some Tasks Like create Item Service and impl Class

---
+++ OnlineP_Food_Delivery_App/src/main/java/com/healthyswad/service/ItemServiceImpl.java
@@ -0,0 +1,26 @@
+package com.healthyswad.service;
+
+import com.healthyswad.exception.ItemException;
+import com.healthyswad.model.Item;
+
+public class ItemServiceImpl implements ItemService {
+
+	@Override
+	public Item addItem(Item item) throws ItemException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item updateItem(Item item) throws ItemException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Item viewItem(Item item) throws ItemException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
