05341bf653b0341a39d99b77e1b162e8bfa6a426
MadhuKinnera <hollypoter5@gmail.com>
2022-11-10 14:34:12 +0530

    packages created

---
+++ projecthealthswad/src/main/java/com/masai/ProjecthealthswadApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjecthealthswadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjecthealthswadApplication.class, args);
+	}
+
+}
