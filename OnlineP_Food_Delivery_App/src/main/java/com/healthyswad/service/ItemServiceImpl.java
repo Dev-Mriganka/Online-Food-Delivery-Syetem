b175f20d85417fc74c557c0d91d9c0f9f2f22b2e
Dev-Mriganka <mrigankamondal10@gmail.com>
2022-11-10 15:14:26 +0530

    DONE

--- OnlineP_Food_Delivery_App/src/main/java/com/healthyswad/service/ItemServiceImpl.java
+++ OnlineP_Food_Delivery_App/src/main/java/com/healthyswad/service/ItemServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+
 
 import com.healthyswad.exception.CategoryException;
 
