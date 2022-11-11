309947c50d81a5d49039d1c96b5f9126bba5a0d3
Ankur0661 <ankurk171@gmail.com>
2022-11-11 13:45:29 +0530

    ss

--- OnlineP_Food_Delivery_App/src/main/java/com/healthyswad/service/BillServiceImpl.java
+++ OnlineP_Food_Delivery_App/src/main/java/com/healthyswad/service/BillServiceImpl.java
@@ -1,5 +1,57 @@
 package com.healthyswad.service;
 
-public class BillServiceImpl {
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.healthyswad.exception.BillException;
+import com.healthyswad.exception.CustomerException;
+import com.healthyswad.model.Bill;
+
+@Service
+public class BillServiceImpl implements BillService{
+
+	@Override
+	public Bill addBill(Bill bill) throws BillException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bill updateBill(Bill bill) throws BillException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bill removeBill(Bill bill) throws BillException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bill viewBill(Bill bill) throws BillException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bill> viewBills(LocalDate startDate, LocalDate endDate) throws BillException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bill> viewBill(String custId) throws BillException, CustomerException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Bill calculateTotalCost(Bill bill) throws BillException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
